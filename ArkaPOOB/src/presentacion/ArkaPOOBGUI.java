54065c5c9f58021ea6552b77793a18943e196266
Carlos <manuel.murillo.1999@gmail.com>
2019-04-15 23:34:26 -0500

    Presentacion

--- ArkaPOOB/src/presentacion/ArkaPOOBGUI.java
+++ ArkaPOOB/src/presentacion/ArkaPOOBGUI.java
@@ -15,9 +15,10 @@ public class ArkaPOOBGUI extends JFrame{
 	private JPanel mazo;
 	private JButton jugar;
 	private JButton salir;
-	private JButton opciones;
 	private JButton instrucciones;
+	private JButton volverInstrucciones;
 	private Container contenedor;
+	
 	public ArkaPOOBGUI() {
 		super("ArkaPOOB");
 		prepareElementos();
@@ -35,6 +36,7 @@ public class ArkaPOOBGUI extends JFrame{
 		mazo = new JPanel(new CardLayout());
 		mazo.add(pantallaInicial, "Inicio");
 		contenedor.add(mazo);
+		prepararInstrucciones();
 	}
 	
 	public void prepareBotones() {
@@ -46,17 +48,14 @@ public class ArkaPOOBGUI extends JFrame{
 	
 	public void creeBotones() {
 		pBotones = new JPanel();
-		pBotones.setLayout(new GridLayout(4,1));
+		pBotones.setLayout(new GridLayout(3,1));
 		jugar = new JButton("Jugar");
 		jugar.setBounds(new Rectangle(50, 20));
-		opciones = new JButton("Opciones");
-		jugar.setBounds(new Rectangle(50, 20));
 		instrucciones = new JButton("Como jugar");
-		jugar.setBounds(new Rectangle(50, 20));
+		instrucciones.setBounds(new Rectangle(30, 20));
 		salir = new JButton("Salir");
-		jugar.setBounds(new Rectangle(50, 20));
+		salir.setBounds(new Rectangle(50, 20));
 		pBotones.add(jugar);
-		pBotones.add(opciones);
 		pBotones.add(instrucciones);
 		pBotones.add(salir);
 		botones.add(pBotones,BorderLayout.CENTER);
@@ -75,7 +74,7 @@ public class ArkaPOOBGUI extends JFrame{
 	}
 	
 	public void prepareElementosVentana() {
-		setSize(500,500);
+		setSize(550,550);
 		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
 		setLocationRelativeTo(null);	
 	}
@@ -94,21 +93,97 @@ public class ArkaPOOBGUI extends JFrame{
 			}
 		};
 		jugar.addActionListener(comenzar);
+		
+		/*ActionListener opcion = new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				//abrirOpciones();
+			}
+		};
+		opciones.addActionListener(opcion);*/
+		
+		ActionListener comoJugar = new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				AbrirInstrucciones();
+			}
+		};
+		instrucciones.addActionListener(comoJugar);
+		
+		ActionListener exit = new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				cerrar();
+			}
+		};
+		salir.addActionListener(exit);
 	}
 	
 	
 	public void juegoNuevo(){
 		System.out.println("Entra en juego");
-		PantallaDeJuego pdj = new PantallaDeJuego();
-		mazo.add(pdj,"juega");
-		CardLayout c1 = (CardLayout)(mazo.getLayout());
-		c1.show(mazo,"juega");
-		pdj.setVisible(true);
+		jugadoresGUI a = new jugadoresGUI();
+		a.setVisible(true);
+	}
+	
+	public void cerrar() {
+		System.exit(0);
 	}
 	
 	private void inicio() {
 		CardLayout c1 = (CardLayout)(mazo.getLayout());
 		c1.show(mazo,"Inicio");
+		setSize(550,550);
+	}
+	
+	private void prepararInstrucciones() {
+		System.out.println("hola");
+		JPanel instruccion = new JPanel(new GridLayout(3,1));
+		
+		mazo.add(instruccion,"Instrucciones");
+		JPanel instruImg = new JPanel(new GridBagLayout());
+		
+		instruccion.add(instruImg);
+		JLabel logo = new JLabel(new ImageIcon(getClass().getResource("/imagenes/ArkFont.png")));
+		instruImg.add(logo);
+		instruImg.setBackground(Color.BLUE);
+		
+		JPanel contenido = new JPanel();
+		JLabel texto = new JLabel("<html><center>Con el ratón moverás la barra hacia los lados con el<br/>"
+				+ "fin de recoger la pelota en su caída y hacerla subir<br/>"
+				+ "de nuevo. El objetivo de este juego es romper todos<br/> "
+				+ "los ladrillos que se encuentran en la parte superior, <br/>"
+				+ "cuando limpies la pantalla pasarás de nivel.</center></html>");
+		texto.setForeground(Color.WHITE);
+		texto.setFont(new Font(null,Font.ITALIC,25));
+		contenido.add(texto);
+		contenido.setBackground(Color.BLACK);
+		instruccion.add(contenido);
+		JPanel boton = new JPanel(new GridLayout(3, 3));
+		boton.setBackground(Color.DARK_GRAY);
+		
+		volverInstrucciones = new JButton("Volver");
+		boton.add(new JLabel());boton.add(new JLabel());boton.add(new JLabel());
+		boton.add(new JLabel());boton.add(volverInstrucciones);boton.add(new JLabel());
+		boton.add(new JLabel());boton.add(new JLabel());boton.add(new JLabel());
+		
+		instruccion.add(boton);
+		//instruccion.setBackground(Color.BLACK);
+		prepareAccionesInstrucciones();
+	}
+	
+	private void prepareAccionesInstrucciones() {
+		volverInstrucciones.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				inicio();
+				
+			}
+		});
+	}
+	
+	public void AbrirInstrucciones(){
+		CardLayout c1 = (CardLayout)(mazo.getLayout());
+		c1.show(mazo,"Instrucciones");
+		setSize(650,550);
 	}
 
 	public static void main(String[] args) {
---
+++ ArkaPOOB/src/presentacion/jugadoresGUI.java
@@ -0,0 +1,174 @@
+package presentacion;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class jugadoresGUI extends JFrame{
+	
+	private JPanel pantalla;
+	private JPanel botones;
+	private Container contenedor;
+	private JButton Jugador;
+	private JButton dosJugadores;
+	private JLabel panelLogo;
+	private JPanel mazo;
+	private JButton jugar1;
+	private JButton jugar2;
+	private JButton volver1;
+	private JButton volver2;
+	private JTextField textNombre;
+	private JComboBox<String> naveColor;
+	private String[] colores = {"Red","Blue","Orange","Green"};
+	
+	public jugadoresGUI() {
+		super("Jugadores");
+		setBackground(Color.BLACK);
+		setSize(485,210);
+		setLocationRelativeTo(null);
+		contenedor = getContentPane();
+		prepareElementos();
+		prepareAcciones();
+	}
+	
+	private void prepareElementos(){
+		pantalla = new JPanel();
+		pantalla.setLayout(new GridLayout(2,1));
+		contenedor.add(pantalla);
+		panelLogo = new JLabel(new ImageIcon(getClass().getResource("/imagenes/jugadores.png")));
+		pantalla.add(panelLogo);
+		pantalla.setBackground(Color.BLACK);
+		botones = new JPanel();
+		botones.setBackground(Color.BLACK);
+		pantalla.add(botones);
+		Jugador = new JButton("1 jugador");
+		dosJugadores = new JButton("2 jugadores");
+		botones.add(Jugador);
+		botones.add(dosJugadores);
+		setIconImage(new ImageIcon(getClass().getResource("/imagenes/icon.png")).getImage());
+		
+		mazo = new JPanel(new CardLayout());
+		contenedor.add(mazo);
+		mazo.add(pantalla,"Inicio");
+		prepareUnJugador();
+		prepareDosJugadores();
+	}
+	
+	private void prepareUnJugador() {
+		JPanel panelUnJugador = new JPanel(new GridLayout(2,1));
+		mazo.add(panelUnJugador,"1Jugador");
+		
+		JPanel unJugadorLogo = new JPanel(new GridBagLayout());
+		panelUnJugador.add(unJugadorLogo);
+		JLabel logo = new JLabel(new ImageIcon(getClass().getResource("/imagenes/ArkFont.png")));
+		unJugadorLogo.add(logo);
+		unJugadorLogo.setBackground(Color.BLACK);
+		
+		JPanel contenido = new JPanel();
+		contenido.setLayout(null);
+		contenido.setBackground(Color.BLACK);
+		
+		JLabel nombre = new JLabel("Nombre:");
+		nombre.setForeground(Color.WHITE);
+		nombre.setBounds(15, 40, 78, 14);
+		contenido.add(nombre);
+		
+		JLabel nave = new JLabel("Color Nave:");
+		nave.setForeground(Color.WHITE);
+		nave.setBounds(15, 85, 78, 14);
+		contenido.add(nave);
+		
+		jugar1 = new JButton("Jugar");
+		jugar1.setBounds(160, 150, 89, 23);
+		contenido.add(jugar1);
+		
+		volver1 = new JButton("Volver");
+		volver1.setBounds(330, 150, 89, 23);
+		contenido.add(volver1);
+		
+		textNombre = new JTextField();
+		textNombre.setBounds(110, 40, 78, 14);
+		contenido.add(textNombre);
+		textNombre.setColumns(10);
+		
+		naveColor = new JComboBox<>(colores);
+		naveColor.setBounds(100, 85, 121, 17);
+		contenido.add(naveColor);
+
+		panelUnJugador.add(contenido);
+	}
+	
+	private void prepareDosJugadores() {
+		JPanel panelUnJugador = new JPanel(new GridLayout(2,1));
+		mazo.add(panelUnJugador,"2Jugador");
+	}
+	
+	private void prepareAcciones() {
+		ActionListener juego = new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				configuracionUnJugador();
+			}
+		};
+		Jugador.addActionListener(juego);
+		
+		ActionListener juego1 = new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				configuracionDosJugador();
+			}
+		};
+		dosJugadores.addActionListener(juego1);
+		
+		ActionListener l = new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				inicio();
+			}
+		};
+		volver1.addActionListener(l);
+		
+		ActionListener jugarUno = new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				jugar(1);
+			}
+		};
+		jugar1.addActionListener(jugarUno);
+	}
+	
+	private void configuracionUnJugador() {
+		setTitle("Configuracion");
+		CardLayout c1 = (CardLayout)(mazo.getLayout());
+		c1.show(mazo,"1Jugador");
+		setSize(600,500);
+		setLocationRelativeTo(null);
+	}
+	
+	private void configuracionDosJugador() {
+		setTitle("Configuracion");
+		CardLayout c1 = (CardLayout)(mazo.getLayout());
+		c1.show(mazo,"2Jugador");
+		setSize(450,725);
+		setLocationRelativeTo(null);
+	}
+	
+	private void inicio() {
+		setTitle("Jugadores");
+		CardLayout c1 = (CardLayout)(mazo.getLayout());
+		c1.show(mazo,"Inicio");
+		setSize(485,210);
+		setLocationRelativeTo(null);
+	}
+	
+	private void jugar(int jugadores) {
+		setTitle("Juego");
+		PantallaDeJuego pdj = null;
+		if(jugadores == 1) {
+			System.out.println("Aqui ");
+			pdj = new PantallaDeJuego();
+			mazo.add(pdj,"juega");
+			CardLayout c1 = (CardLayout)(mazo.getLayout());
+			c1.show(mazo,"juega");
+			pdj.setVisible(true);
+		}
+	}
+	
+}
